8149a6be5951eca55b8d77799521af32fb0dba28
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-05-08 15:35:17 -0400

    Storing Data in Components with the Data Method

---
+++ script.js
@@ -0,0 +1,19 @@
+// var data = { status: 'Critical'};
+
+Vue.component('my-cmp', {
+  data: function() {
+    return {
+      status: 'Critical'
+    };
+  },
+  template: '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal';
+    }
+  }
+});
+
+new Vue({
+  el: '#app'
+})
